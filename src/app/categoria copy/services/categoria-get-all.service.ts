bdd099e479316e3814f1b634e8bc7bb68ae81028
Henry <henry.pereira@univille.br>
2023-06-29 17:01:54 -0300

    teste

---
+++ src/app/categoria copy/services/categoria-get-all.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { map, Observable } from 'rxjs';
+import { CategoriaGetInterface } from '../interfaces/categoria-get-interface';
+@Injectable({providedIn: 'root'})
+export class CategoriaGetAllService {
+  constructor(private http: HttpClient) { }
+  get(): Observable<CategoriaGetInterface[]> {
+    return this.http.get<CategoriaGetInterface[]>(`http://localhost:8080/categoria`)
+  }
+}
---
+++ src/app/categoria copy/services/categoria-update.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoriaPostInterface } from '../interfaces/categoria-post-interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaUpdateService {
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+
+  constructor(private http: HttpClient) { }
+
+  public update(produto: CategoriaPostInterface, id: string): Observable<CategoriaPostInterface>{
+    return this.http.put<CategoriaPostInterface>(`http://localhost:8080/categoria/${id}`, produto, this.httpOptions);
+  }
+}
