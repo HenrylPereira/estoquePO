e95b5742625e3aa4be57f983a222e65da16a1e72
Henry <henry.pereira@univille.br>
2023-07-06 17:11:51 -0300

    ajustes

--- src/app/produto/components/produto-grid/produto-grid.component.ts
+++ src/app/produto/components/produto-grid/produto-grid.component.ts
@@ -6,6 +6,7 @@ import { ProdutoGetInterface } from '../../interfaces/produto-get-interface';
 import { ProdutoDeleteService } from '../../services/produto-delete.service';
 import { CategoriaGetAllService } from 'src/app/categoria/services/categoria-get-all.service';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ProdutoUpdateService } from '../../services/produto-update.service';
 
 @Component({
   selector: 'app-produto-grid',
@@ -18,11 +19,15 @@ export class ProdutoGridComponent implements OnInit {
   public ModalProduto !: PoModalAction
   public categorias: any;
   public form!: FormGroup;
+  public editMode = false;
+  public produtoId!: string;
 
   constructor(
     private produtoGetAllService: ProdutoGetAllService,
     private categoriaGetAllService: CategoriaGetAllService,
     private produtoCreateService: ProdutoCreateService,
+    private produtoUpdateService: ProdutoUpdateService,
+    private produtoDeleteService: ProdutoDeleteService,
     private formBuilder: FormBuilder
     ) { }
 
@@ -62,12 +67,14 @@ export class ProdutoGridComponent implements OnInit {
   }
 
   public actions: Array<PoTableAction> = [
-    { icon: 'po-icon po-icon-edit', label: 'Alterar' },
+    { icon: 'po-icon po-icon-edit', label: 'Alterar', action: this.editar.bind(this) },
     { icon: 'po-icon po-icon-delete', label: 'Deletar', action: this.deletar.bind(this) }
   ];
 
   private deletar(item: ProdutoGetInterface){
-    //this.produtoDeleteService.delete(item.id).subscribe();
+    this.produtoDeleteService.delete(item.id).subscribe(()=>{
+      this.popularTable();
+    });
   }
 
   columns: Array<PoTableColumn> = [
@@ -87,13 +94,39 @@ export class ProdutoGridComponent implements OnInit {
     label: 'Confirmar'
   };
 
+  public editar(produto: ProdutoGetInterface){
+    this.poModal.open();
+    this.preencherForm(produto);
+    this.editMode = true;
+    this.produtoId = produto.id;
+  }
+
+  private preencherForm(event: ProdutoGetInterface){
+    this.form.get('dataEntrada')?.disable();
+    this.form.get('dataEntrada')?.setValue(event.dataEntrada);
+    this.form.get('categoriaId')?.setValue(event.categoria.id);
+    this.form.get('titulo')?.setValue(event.titulo);
+    this.form.get('quantidade')?.setValue(event.quantidade);
+    this.form.get('valor')?.setValue(event.valor);
+    this.form.get('quantidadeMin')?.setValue(event.quantidadeMin);
+    this.form.get('quantidadeMax')?.setValue(event.quantidadeMax);
+  }
+
   public salvar() {
     if (this.form.valid) {
-      this.produtoCreateService.create(this.form.value).subscribe(() => {
-        this.popularTable();
-        this.poModal.close();
-        this.form.reset();
-      });
+      if(this.editMode){
+        this.produtoUpdateService.update(this.form.value, this.produtoId).subscribe(() => {
+          this.popularTable();
+          this.poModal.close();
+          this.form.reset();
+        });
+      }else{
+        this.produtoCreateService.create(this.form.value).subscribe(() => {
+          this.popularTable();
+          this.poModal.close();
+          this.form.reset();
+        });
+      }
     }
   }
 
